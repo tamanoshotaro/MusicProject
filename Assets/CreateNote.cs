d8b952944b282cb47c9c9f853ba821b889157afe
tamanoshotaro <darkbring.master31730202@docomo.ne.jp>
2017-04-06 21:45:51 +0900

    4.6

---
+++ Assets/CreateNote.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNote : MonoBehaviour {
+
+	public GameObject note;
+
+	public Vector3 posit = new Vector3 (-10, 0, 0);
+
+	private int index = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (Beat.timer >= Beat.timing [index] - 1) {
+			CreateNote ();
+			index++;
+		}
+		*/
+	}
+
+
+	public void Create(){
+		Instantiate (note, this.transform.position, this.transform.rotation);
+	}
+
+
+}
